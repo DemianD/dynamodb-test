8b21fdb253be371da04767e7c48e7cde9326d4a7
Demian Dekoninck <demian@live.be>
2021-01-18 20:19:38 +0100

    add link to docs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 
+// docs: https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-dynamodb/index.html
 console.log(DynamoDBClient);
